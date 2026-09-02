2285a53cff469907ebbe3b6f563324372365fd51
leticia <lima.leticia2@escolara.ifrn.edu.br>
2026-09-02 15:31:55 -0300

    escopo e hoisting

--- scrip.js
+++ scrip.js
@@ -6,8 +6,26 @@ let altura;
 function calcIdade(ano = 2026){
     let idade = ano - nasc;
     console.log(`idade dentro da funcao:, ${idade}`);
+    let menor;
+    if(idade < 18){
+        menor = true
+    }
+    else {
+        menor = false
+    }
+
     return idade;
 }
+retorno = calcIdade()
+a= retorno[0]
+b= retorno[1]
+
+if(retorno[1]){
+    alert(`idade: ${retorno[0]}, voce é menor de idade`)
+}else{
+    alert(`idade: ${retorno[0]}, voce é maior de idade`)
+}
+
 alert(`idade fora da funcao: ${calcIdade()}`);
 /*console.log(`idade fora da função: ${idade}`);
 */
